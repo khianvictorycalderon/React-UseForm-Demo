fd0fc2bf57563f463b1180384f3cf6c2d5321af7
khian calderon <ckhianvictory@gmail.com>
2025-08-07 16:26:23 +0800

    Added register

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { type UseFormRegisterReturn } from "react-hook-form";
+
 interface FeedbackProps {
   type: "warning" | "error";
   message: string;
@@ -9,12 +12,13 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   feedbackColor?: {
     warning?: string;
     error?: string;
-  }
+  };
   additionalClassName?: {
     label?: string;
     input?: string;
     feedback?: string;
-  }
+  };
+  register?: UseFormRegisterReturn;
 }
 
 export const Input: React.FC<InputProps> = ({
@@ -22,6 +26,7 @@ export const Input: React.FC<InputProps> = ({
   feedback,
   feedbackColor,
   additionalClassName,
+  register,
   ...props
 }) => {
   const resolvedFeedbackColor =
@@ -35,15 +40,16 @@ export const Input: React.FC<InputProps> = ({
     <div>
       {label && (
         <label
-            className={`pl-2 block text-sm font-medium mb-1 text-zinc-100 ${additionalClassName?.label ?? ""}`}
-            htmlFor={props.id}
+          className={`pl-2 block text-sm font-medium mb-1 text-zinc-100 ${additionalClassName?.label ?? ""}`}
+          htmlFor={props.id}
         >
-            {label}
+          {label}
         </label>
       )}
       <input
         className={`w-full px-4 py-2 bg-zinc-700 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${additionalClassName?.input ?? ""}`}
         placeholder={`Enter ${label?.toLowerCase()}...`}
+        {...register}
         {...props}
       />
       {feedback && (
